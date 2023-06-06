a54c28b40b1a56895afb0c5414f47e0c67c139b3
unknown <129381997+DIYOR77@users.noreply.github.com>
2023-06-06 16:54:54 +0500

    Finding the number of rows

--- homework3.6/homework3.6/Program.cs
+++ homework3.6/homework3.6/Program.cs
@@ -4,7 +4,18 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string path = "C:\\Project\\Lesson.txt";
+            int count = 0;
+            using (StreamReader sr = new StreamReader(path))
+            {
+                while (sr.ReadLine() != null)
+                {
+                    count++;
+                }
+            }
+            Console.WriteLine($" Qatorlar soni: {count}");
+
         }
     }
 }
+    
